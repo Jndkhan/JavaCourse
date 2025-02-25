09816f1e87de1d750086ef7dfec04ea129e3f485
JunaidKhan <jndkhan8arkotech@gmail.com>
2025-02-25 12:54:44 +0500

    Added new file for java configuration

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,30 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.naming.Context;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        /// Java Base Configuration
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean("desktop",Desktop.class);
+        dt.myLap();
+
+
+
+        /// through Spring xml file
+       /* ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj = (Alien) context.getBean("alien");
         obj.code();
-        obj.getAge();
+        obj.getAge();*/
 
         /*Laptop laptop = (Laptop) context.getBean("laptop");
         laptop.myLap();*/
--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -1,5 +1,16 @@
 package org.example.config;
 
+import org.example.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+
+    @Bean(name = "desktop")
+    public Desktop desktop1(){
+        return new Desktop();
+    }
 
 }
